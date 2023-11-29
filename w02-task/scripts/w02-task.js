54ad17a14f1998396ce944882c5d8e35beeb9f6f
trevheg <trevorhegstrom@gmail.com>
2023-11-28 20:21:20 -0800

    Week 2 tasks

---
+++ w02-task/scripts/w02-task.js
@@ -0,0 +1,39 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+const fullName = "Trevor Hegstrom";
+const currentYear = 2023;
+const profilePicture = "images/me-image.jpg"
+
+/* Step 3 - Element Variables */
+
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+let imageElement = document.createElement('img');
+imageElement = document.querySelector("img");
+
+/* Step 4 - Adding Content */
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = currentYear;
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt', `Profile image of ${fullName}`);
+
+/* Step 5 - Array */
+let favoriteFoods = ['pizza', 'doughnuts', 'curry'];
+foodElement.innerHTML = favoriteFoods;
+let newFood = "cheesecake";
+favoriteFoods.push(newFood);
+foodElement.innerHTML += `<br> ${favoriteFoods}`;
+favoriteFoods.shift(0);
+foodElement.innerHTML += `<br> ${favoriteFoods}`;
+favoriteFoods.pop();
+foodElement.innerHTML += `<br> ${favoriteFoods}`;
+
+
+
+
+
+
