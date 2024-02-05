d2980c8c38b4b50f4f86880f92eafcf3d015e092
trevheg <trevorhegstrom@gmail.com>
2024-02-05 13:17:55 -0800

    week 4 loops

---
+++ loops.js
@@ -0,0 +1,71 @@
+// loops.js
+myInfo = {
+  name: "Brother T",
+  photo: "images/photo.jpg",
+  favoriteFoods: ["Fettucini", "Steak", "Chicken", "Shrimp", "Baked Potato"],
+  hobbies: ["Reading", "Fishing", "Camping"],
+  placesLived: [
+    {
+      place: "Rexburg, ID",
+      length: "5 years",
+    },
+    {
+      place: "Ammon, ID",
+      length: "3 years",
+    },
+    {
+      place: "Sandy, UT",
+      length: "1 year",
+    },
+  ],
+};
+// Step 4: For each favorite food in the favoriteFoods property, create an HTML <li> element and place its value in the <li> element
+// myInfo.favoriteFoods.forEach(food => {
+//     let favoriteFood = document.createElement("li");
+//     favoriteFood.textContent = food;
+//     document.querySelector("#favorite-foods").appendChild(favoriteFood);
+// });
+
+// myInfo.favoriteFoods.map((food) => {    
+//     let favoriteFood = document.createElement("li");
+//     favoriteFood.textContent = food;
+//     document.querySelector("#favorite-foods").appendChild(favoriteFood);
+// });
+
+// document.querySelector('#favorite-foods').innerHTML = (myInfo.favoriteFoods.map((food) => `<li>${food}</li>`)).join("");
+// remember that readable code means maintainable code
+
+const htmlEmbeded = (myString) => {
+    return `<li>${myString}</li>`;
+}
+const placeEmbedded = (place) => {
+    return `<dt>${place.place}</dt><dd>${place.length}</dd>`;
+}
+const listToHtml = (list, callback) => {
+    const htmlList = list.map((item) => {
+        return callback(item);
+    })
+    return htmlList.join("");
+}
+document.querySelector('#favorite-foods').innerHTML = listToHtml(myInfo.favoriteFoods, htmlEmbeded);
+document.querySelector('#places-lived').innerHTML = listToHtml(myInfo.placesLived, placeEmbedded)
+
+
+
+// let favoriteFood1 = document.createElement("li");
+// favoriteFood1.textContent = myInfo.favoriteFoods[0];
+
+// let favoriteFood2 = document.createElement("li");
+// favoriteFood2.textContent = myInfo.favoriteFoods[1];
+
+// let favoriteFood3 = document.createElement("li");
+// favoriteFood3.textContent = myInfo.favoriteFoods[2];
+
+// let favoriteFood4 = document.createElement("li");
+// favoriteFood4.textContent = myInfo.favoriteFoods[3];
+
+// Step 5: Append the <li> elements created above as children of the HTML <ul> element with an ID of favorite-foods
+// document.querySelector("#favorite-foods").appendChild(favoriteFood1);
+// document.querySelector("#favorite-foods").appendChild(favoriteFood2);
+// document.querySelector("#favorite-foods").appendChild(favoriteFood3);
+// document.querySelector("#favorite-foods").appendChild(favoriteFood4);
--- sandbox/sandbox.js
+++ sandbox/sandbox.js
@@ -1,23 +1,50 @@
-const buttonElement = document.getElementById("submitButton");
+// const buttonElement = document.getElementById("submitButton");
 
-function copyInput(event) {
-  // take a look at the event!
-  console.log(event);
-  const inputElement = document.getElementById("inputBox");
-  const outputElement = document.getElementById("output");
-  outputElement.innerHTML = inputElement.value;
+// function copyInput(event) {
+//   // take a look at the event!
+//   console.log(event);
+//   const inputElement = document.getElementById("inputBox");
+//   const outputElement = document.getElementById("output");
+//   outputElement.innerHTML = inputElement.value;
+// }
+// buttonElement.addEventListener("click", copyInput);
+// const log = document.querySelector("#log");
+
+// document.addEventListener("keydown", logKey);
+
+// function logKey(e) {
+//   // how do we know which key was pressed?
+//   console.log(e);
+//   console.log(e.key);
+//   // checkout e.code, e.key, and e.keyCode
+//   // what is the difference?
+//   // the following will make the "log" paragraph equal to the key pressed
+//   log.innerHTML = e.key;
+// }
+
+const DAYS = 6;
+const LIMIT = 30;
+let studentReport = [11, 42, 33, 64, 29, 37, 44];
+
+for (let i=0; i < studentReport.length; i++) {
+  if (studentReport[i] < LIMIT) {
+    console.log(studentReport[i]);
+  }
 }
-buttonElement.addEventListener("click", copyInput);
-const log = document.querySelector("#log");
-
-document.addEventListener("keydown", logKey);
-
-function logKey(e) {
-  // how do we know which key was pressed?
-  console.log(e);
-  console.log(e.key);
-  // checkout e.code, e.key, and e.keyCode
-  // what is the difference?
-  // the following will make the "log" paragraph equal to the key pressed
-  log.innerHTML = e.key;
+let itemNo = 0;
+while (itemNo < studentReport.length) {
+  if (studentReport[itemNo] < LIMIT) {
+    console.log(studentReport[itemNo]);
+  }
+  itemNo ++;
+}
+studentReport.forEach(n => {
+  if (n < LIMIT) {
+    console.log(n);
+  }
+})
+for (const i in studentReport) {
+  if (studentReport[i] < LIMIT) {
+    console.log(studentReport[i]);
+  }
 }
