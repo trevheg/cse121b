77a2cae0199cc24fe9d8f8757c88d4765898f2ce
trevheg <trevorhegstrom@gmail.com>
2024-02-12 10:57:47 -0800

    pokelist crap

--- learning/fetch.js
+++ learning/fetch.js
@@ -1,19 +1,47 @@
 // fetch.js
 const url = "https://pokeapi.co/api/v2/pokemon/ditto";
+const urlList = "https://pokeapi.co/api/v2/pokemon";
 let results = null;
 async function getPokemon(url) {
-  const response = await fetch(url);
-  //check to see if the fetch was successful
-  if (response.ok) {
-    // the API will send us JSON...but we have to convert the response before we can use it
-    // .json() also returns a promise...so we await it as well.
-    const data = await response.json();
-    doStuff(data);
-  }
+    const response = await fetch(url);
+    //check to see if the fetch was successful
+    if (response.ok) {
+        // the API will send us JSON...but we have to convert the response before we can use it
+        // .json() also returns a promise...so we await it as well.
+        const data = await response.json();
+        doStuff(data);
+    }
 }
 function doStuff(data) {
-  results = data;
-  console.log("first: ", results);
+    results = data;
+    console.log("first: ", results);
+    const outputElement = document.querySelector("#output");
+    const pokeHtml = `<h2>${results.name}</h2>
+                    <img src="${results.sprites.front_default}" alt="image of ${results.name}">`
+
+    outputElement.innerHTML = pokeHtml;
+}
+function doStuffList(data) {
+    console.log(data);
+    const pokeListElement = document.querySelector("#outputList");
+    const pokeList = data.results;
+    // for (const poke in pokeList) {
+    //     const pokeHtml = `<li>${poke.name}</li>;`
+    //     pokeListElement.innerHTML += pokeHtml;
+    // }
+      pokeList.forEach((currentItem) => {
+        const html = `<li>${currentItem.name}</li>`;
+        // note the += here...
+        pokeListElement.innerHTML += html;
+      });
+}
+async function getPokemonList(url) {
+    const response = await fetch(url);
+    if (response.ok) {
+        data = await response.json();
+        doStuffList(data);
+    }
 }
 getPokemon(url);
+getPokemonList(urlList);
 console.log("second: ", results);
