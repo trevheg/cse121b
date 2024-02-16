24d027b1e3b1f2af8385567b567b55a460d94fdd
trevheg <trevorhegstrom@gmail.com>
2024-02-15 16:56:23 -0800

    Final Project v1 (no order guarantee)

--- scripts/project.js
+++ scripts/project.js
@@ -2,33 +2,52 @@
 
 // Global Variables
 const pokemonElement = document.querySelector("#pokemon");
-const pokemonUrl = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=9"
+const pokemonUrl = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=1000"
 let pokemonList = [];
 
 // Displays list of Pokemon
 const displayPokemon = async (pokemonList) => {
     // console.log(pokemonList);
-    const article = document.createElement("article");
+    reset();
     const pokeList = pokemonList.results;
+
+
     pokeList.forEach(async (poke) => {
         let h3 = document.createElement("h3");
         let img = document.createElement("img");
-        h3.innerText = `${poke.name}`;
-        // console.log(poke.url);
 
-        // get individual pokemon api
+        // get current pokemon api json
         const response = await fetch(poke.url);
         const pokeData = await response.json();
-        console.log(pokeData);
 
+        // get the types of the current pokemon
+        let types = [];
+        pokeData.types.forEach((type) => {
+            types.push(type.type.name)
+        })
 
+        const userSelection = document.querySelector("#typeFilter").value;
 
-        // console.log(await data.sprites.front_default)
-        img.src = `${pokeData.sprites.front_default}`;
-        img.alt = `${poke.name}`;
-        article.appendChild(h3);
-        article.appendChild(img);
-        pokemonElement.appendChild(article);
+        // print each pokemon of the selected type to the screen
+        if (userSelection && userSelection != "all") {
+            if (types.includes(document.querySelector("#typeFilter").value)) {
+                showPoke(pokeData);
+            }
+        } else if (!userSelection || userSelection == "all") {
+            showPoke(pokeData);
+        }
+
+        // adds the selected pokemon to the web page
+        function showPoke(pokeData) {
+            const article = document.createElement("article");
+            h3.innerText = `${toTitleCase(pokeData.name)}`;
+            img.src = `${pokeData.sprites.front_default}`;
+            img.alt = `${pokeData.name}`;
+            article.appendChild(h3);
+            article.appendChild(img);
+            pokemonElement.appendChild(article);
+
+        }
     })
 }
 
@@ -39,67 +58,19 @@ const getPokemonList = async (url) => {
     displayPokemon(pokemonList);
 }
 
-
-// // gets pokemon array from api
-// const getPokemonData = async (url) => {
-//     const response = await fetch(url);
-//     // if (response.ok) {
-//     const data = await response.json();
-//     // console.log(data.sprites.front_default);
-//     return data;
-//     // }
-// }
 /* reset Function */
 const reset = function () {
     pokemonElement.innerHTML = "";
 }
-function filterByType(list) {
-
-}
-function filterPokemon(pokemonList) {
-    // reset();
-    filter = document.querySelector("#typeFilter").value;
-    switch (filter) {
-        case "bug":
-            break;
-        case "dark":
-            break;
-        case "dragon":
-            break;
-        case "electric":
-            break;
-        case "fairy":
-            break;
-        case "fighting":
-            break;
-        case "fire":
-            break;
-        case "flying":
-            break;
-        case "ghost":
-            break;
-        case "grass":
-            break;
-        case "ground":
-            break;
-        case "ice":
-            break;
-        case "normal":
-            break;
-        case "poison":
-            break;
-        case "psychic":
-            break;
-        case "rock":
-            break;
-        case "steel":
-            break;
-        case "water":
-            break;
-        case "all":
-            break;
-    }
+// converts a string to title case
+function toTitleCase(str) {
+    return str.replace(
+        /\w\S*/g,
+        function (txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }
+    );
 }
 // event listener
-document.querySelector("#typeFilter").addEventListener("change", () => (filterPokemon(pokemonList)));
+document.querySelector("#typeFilter").addEventListener("change", () => (displayPokemon(pokemonList)));
 getPokemonList(pokemonUrl);
